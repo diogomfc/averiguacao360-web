d71bb8bc1b2da87d4a6d6a7c245bf77ba9ef02c3
Diogo Silva <diogomfc@hotmail.com>
2024-03-02 11:26:03 -0300

    feat: instalalando shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import './global.css'
+
 export function App() {
   return (
-    <div className="flex">
+    <div className="flex h-screen items-center justify-center">
       <h1>Averiguação 360</h1>
     </div>
   )
