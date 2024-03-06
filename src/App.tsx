b8110afb3b588fc98dd2d7163abc998eac6c81cd
Diogo Silva <diogomfc@hotmail.com>
2024-03-06 10:23:41 -0300

    feat: configurando API clinet - Axios e React-Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import './global.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
+import { queryClient } from './lib/react-query'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="Smart Hub | %s" />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </HelmetProvider>
   )
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { Averiguacao360Layout } from './page/_layouts/app/layout-averiguação360'
 import { HubLayout } from './page/_layouts/app/layout-hub'
 import { AuthLayout } from './page/_layouts/layout-auth'
+import { NotFound } from './page/404'
 import { DashboardAveriguacao360 } from './page/app/averiguacao360/dashboard'
 import { Hub } from './page/app/hub'
 import { SignIn } from './page/auth/singIn'
@@ -17,11 +18,13 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <HubLayout />,
+    errorElement: <NotFound />,
     children: [{ path: '/Hub', element: <Hub /> }],
   },
   {
     path: '/',
     element: <Averiguacao360Layout />,
+    errorElement: <NotFound />,
     children: [
       { path: '/averiguacao360', element: <DashboardAveriguacao360 /> },
     ],
